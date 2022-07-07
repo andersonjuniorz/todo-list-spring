ea545846d0e848e086f131af53ac1b78b1771337
andersonjuniorz <dev.andersonjunior@gmail.com>
2022-07-07 09:31:58 -0300

    Adicionando arquivos de configuracao

---
+++ src/test/java/com/andersonjunior/todolistspring/TodolistSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersonjunior.todolistspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
