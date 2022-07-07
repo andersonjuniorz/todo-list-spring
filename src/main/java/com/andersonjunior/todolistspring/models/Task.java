941def9569e4c9aad5f8ae00df8b55ddea3c4fdb
andersonjuniorz <dev.andersonjunior@gmail.com>
2022-07-07 09:40:47 -0300

    Adicionando task model

---
+++ src/main/java/com/andersonjunior/todolistspring/models/Task.java
@@ -0,0 +1,36 @@
+package com.andersonjunior.todolistspring.models;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "tb_tasks")
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false)
+	private LocalDateTime deadLine;
+
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime UpdatedAt;
+}
